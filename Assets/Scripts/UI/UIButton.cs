e29db231e48e5b5d4292f4457720cc0b19caafb9
akaishi takeshi <t_akaishi@cgate.jp>
2016-12-31 18:48:22 +0900

    namespace change

--- Assets/Scripts/UI/UIButton.cs
+++ Assets/Scripts/UI/UIButton.cs
@@ -1,36 +1,42 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UniRx;
 
-[RequireComponent (typeof (UnityEngine.UI.Button))]
-public class UIButton : MonoBehaviour {
+namespace UI
+{
+	[RequireComponent (typeof (UnityEngine.UI.Button))]
+	public class UIButton : MonoBehaviour {
 
-	UnityEngine.UI.Button button {
-		get {
-			return GetComponent<UnityEngine.UI.Button>();
+		UnityEngine.UI.Button button {
+			get {
+				return GetComponent<UnityEngine.UI.Button>();
+			}
+		}
+
+		/// <summary>
+		/// Awake is called when the script instance is being loaded.
+		/// </summary>
+		void Awake()
+		{
+			// var path = Path.Combine(Application.persistentDataPath, "user.bin");
+			var builder = new FlatBuffers.FlatBufferBuilder(1);
+			var data = Data.User.CreateUser(builder);
+			
+			button.onClick.AsObservable()
+				.Select(_ => 1)
+				.Scan(0, (element, acc) => element + acc)
+				.Subscribe (count => Debug.Log("count:" + count))
+				.AddTo(gameObject);
+		}
+
+		// Use this for initialization
+		void Start () {
+			
+		}
+		
+		// Update is called once per frame
+		void Update () {
+			
 		}
 	}
 
-	/// <summary>
-	/// Awake is called when the script instance is being loaded.
-	/// </summary>
-	void Awake()
-	{
-		button.onClick.AsObservable()
-			.Select(_ => 1)
-			.Scan(0, (element, acc) => element + acc)
-			.Subscribe (count => Debug.Log("count:" + count))
-            .AddTo(gameObject);
-	}
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
