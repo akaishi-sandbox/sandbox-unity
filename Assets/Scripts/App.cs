6e78bed450cb589a2f2b363ae95682308b9a8a15
akaishi <galigalikun@gmail.com>
2017-08-11 16:30:06 +0900

    エントリーポイントをappに

---
+++ Assets/Scripts/App.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class App : MonoBehaviour
+{
+
+    /// <summary>
+    /// Awake is called when the script instance is being loaded.
+    /// </summary>
+    void Awake()
+    {
+        ServiceLocator.Instance.Register<DataStore>();
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        ServiceLocator.Instance.Get<DataStore>().Ass();
+        aaa();
+
+    }
+
+    async Task<string> aaa()
+    {
+        await Task.Delay(1000);
+
+
+        return "end";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/ServiceLocator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class ServiceLocator
+{
+
+    static readonly Lazy<ServiceLocator> instance = new Lazy<ServiceLocator>(() => new ServiceLocator());
+
+    public static ServiceLocator Instance => instance.Value;
+
+    readonly Dictionary<Type, Lazy<object>> registeredServices = new Dictionary<Type, Lazy<object>>();
+
+    public void Register<T>() where T : new()
+    {
+        registeredServices[typeof(T)] = new Lazy<object>(() => Activator.CreateInstance(typeof(T)));
+    }
+
+    public T Get<T>() where T : class
+    {
+        Lazy<object> service;
+        if (registeredServices.TryGetValue(typeof(T), out service))
+        {
+            return service.Value as T;
+        }
+
+        return null;
+    }
+}
