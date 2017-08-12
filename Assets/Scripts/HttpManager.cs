dc630ecfc8645a67be8bd58b36c0625dc66ce06c
akaishi <galigalikun@gmail.com>
2017-08-12 20:27:23 +0900

    socket試し

---
+++ Assets/Scripts/HttpManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System.Net;
+using System.Net.Sockets;
+
+public class HttpManager : Singleton<HttpManager>
+{
+
+    void aaaa()
+    {
+        var client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        var endpoint = new IPEndPoint(0, 0);
+        client.BeginConnect(endpoint, (res) =>
+        {
+            // callback
+        }, null);
+    }
+}
