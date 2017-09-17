7c885c6976a0349cc71bc0b38f823b93fe2d81f9
akaishi <galigalikun@gmail.com>
2017-09-17 22:35:34 +0900

    取得できるようになった！

--- Assets/Scripts/Editor/SheetTest.cs
+++ Assets/Scripts/Editor/SheetTest.cs
@@ -10,6 +10,7 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Net;
 
 using Google.Apis.Json;
 using Newtonsoft.Json;
@@ -25,8 +26,12 @@ public class SheetTest
     static string[] Scopes = { SheetsService.Scope.SpreadsheetsReadonly };
     static string ApplicationName = "Google Sheets API .NET Quickstart";
 
-    public async static void Sheets()
+    public static void Sheets()
     {
+        ServicePointManager.ServerCertificateValidationCallback = (sender, certificate, chain, sslPolicyErrors) =>
+        {
+            return true;
+        };
         UserCredential credential;
 
         using (var stream =
@@ -48,12 +53,12 @@ public class SheetTest
             // }
 
 
-            credential = await GoogleWebAuthorizationBroker.AuthorizeAsync(
+            credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
                 GoogleClientSecrets.Load(stream).Secrets,
                 Scopes,
                 "user",
                 CancellationToken.None,
-                new FileDataStore(credPath, true)); // .Result;
+                new FileDataStore(credPath, true)).Result;
             UnityEngine.Debug.Log($"Credential file saved to: {credPath}");
         }
 
@@ -76,11 +81,10 @@ public class SheetTest
         IList<IList<Object>> values = response.Values;
         if (values != null && values.Count > 0)
         {
-            UnityEngine.Debug.Log("Name, Major");
             foreach (var row in values)
             {
                 // Print columns A and E, which correspond to indices 0 and 4.
-                UnityEngine.Debug.Log($"{row[0]}, {row[4]}");
+                UnityEngine.Debug.Log($"{row.ElementAtOrDefault(0)}, {row.ElementAtOrDefault(1)}");
             }
         }
         else
