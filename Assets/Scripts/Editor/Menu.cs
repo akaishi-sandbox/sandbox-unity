d6f2517a5cf55ec7249f6d3158e9114d0a054384
akaishi <galigalikun@gmail.com>
2019-07-30 12:23:38 +0900

    namespace

--- Assets/Scripts/Editor/Menu.cs
+++ Assets/Scripts/Editor/Menu.cs
@@ -3,48 +3,52 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
-public static class Menu
+namespace Admin
 {
-    [MenuItem("Test/Open/Admin", false, 9)]
-    public static void OpenAdminWindow()
+public static class Menu
     {
-        EditorWindow.GetWindow<AdminWindow>(false, "Admin", true).Show();
-    }
-
-    [MenuItem("Test/Debug/All", false, 9)]
-    public static void ChangeHideFlag()
-    {
-        foreach (Transform obj in GameObject.FindObjectsOfType(typeof(Transform)))
+        [MenuItem("Test/Open/Admin", false, 9)]
+        public static void OpenAdminWindow()
         {
-            Debug.Log($"obj:{obj.name}");
-            if (obj.gameObject.hideFlags == HideFlags.None)
+            EditorWindow.GetWindow<AdminWindow>(false, "Admin", true).Show();
+        }
+
+        [MenuItem("Test/Debug/All", false, 9)]
+        public static void ChangeHideFlag()
+        {
+            foreach (Transform obj in GameObject.FindObjectsOfType(typeof(Transform)))
             {
-                obj.gameObject.hideFlags = HideFlags.HideInInspector;
+                Debug.Log($"obj:{obj.name}");
+                if (obj.gameObject.hideFlags == HideFlags.None)
+                {
+                    obj.gameObject.hideFlags = HideFlags.HideInInspector;
+                }
+                else
+                {
+                    obj.gameObject.hideFlags = HideFlags.None;
+                }
+                EditorApplication.DirtyHierarchyWindowSorting();
             }
-            else
+        }
+
+        [MenuItem("Test/Shotcut/ActiveToggle _a")]
+        public static void ActiveToggle()
+        {
+            foreach (GameObject obj in Selection.objects)
             {
-                obj.gameObject.hideFlags = HideFlags.None;
+                Undo.RecordObject(obj, "Undo ActiveToggle");
+                obj.SetActive(!obj.activeSelf);
             }
-            EditorApplication.DirtyHierarchyWindowSorting();
+        }
+
+        /// <summary>
+        /// ゲームオブジェクトのapply
+        /// </summary>
+        [MenuItem("Test/Shotcut/PrefabApply &a")]
+        static void PrefabApply()
+        {
+            EditorApplication.ExecuteMenuItem("GameObject/Apply Changes To Prefab");
         }
     }
 
-    [MenuItem("Test/Shotcut/ActiveToggle _a")]
-    public static void ActiveToggle()
-    {
-        foreach (GameObject obj in Selection.objects)
-        {
-            Undo.RecordObject(obj, "Undo ActiveToggle");
-            obj.SetActive(!obj.activeSelf);
-        }
-    }
-
-    /// <summary>
-    /// ゲームオブジェクトのapply
-    /// </summary>
-    [MenuItem("Test/Shotcut/PrefabApply &a")]
-    static void PrefabApply()
-    {
-        EditorApplication.ExecuteMenuItem("GameObject/Apply Changes To Prefab");
-    }
 }
